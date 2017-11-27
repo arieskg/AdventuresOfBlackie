2f1a512fe0060c6e32b6d079fa331f22401551e2
Kevin Gao <milokgao@gmail.com>
2017-11-27 13:31:02 -0800

    first pub
    yes

---
+++ src/main/java/FirstScenario.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class FirstScenario {
+    static String[] firstScenario = new String[] {"Meow"
+                                            ,"Jump on bed"
+                                            ,"Poop on bed"
+                                            ,"Scratch closed door"};
+
+
+    public static void main(String[] args) {
+        System.out.println("Blacky just woke up on the bed and is hungry. Kenny and Jing are asleep");
+        System.out.println(" /\\_/\\");
+        System.out.println("( o.o )");
+        System.out.println(" > ^ <");
+        System.out.println("========M E N U========");
+
+        for (int i = 0; i < firstScenario.length; i++){              // Print first scenarios
+            System.out.println((i+1)+". " + firstScenario[i]);
+        }
+
+        int meowCounter = 0;
+        int levelCounter = 0;
+        while (levelCounter != 1) {
+            Scanner reader = new Scanner(System.in);                    // Read from System.in
+            System.out.println("Choose an action: ");
+            int userChoice = reader.nextInt();                          // Scans the next token of the input as an int.
+            if (userChoice == 1) {
+                System.out.println("Meow!");
+                meowCounter++;
+            }
+            if (userChoice == 2) {
+                System.out.println("小黑烦死了！Jing goes back to sleep");
+            }
+            if (userChoice == 3) {
+                System.out.println("卧槽！好你竟敢在床上大便！这两个星期别想吃tuna了！");
+                System.out.println("Game over!");
+                break;
+            }
+            if (userChoice == 4) {
+                if (meowCounter > 5) {
+                    levelCounter += 1;
+                    System.out.println("Jing's awake, opens door,");
+                    System.out.print("Next Level begins.......");
+                }
+
+            }
+        }
+
+
+    }
+}
